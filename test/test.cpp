f499d7e617f5466cc652b9ac15d88b754d1a2ef4
Caleb Mak <calebmak@gmail.com>
2023-11-02 15:34:01 -0700

    give the tests different names

--- test/test.cpp
+++ test/test.cpp
@@ -32,12 +32,12 @@ TEST(TriangleTests, testCreation) {
 }
 
 // 3 failing tests
-TEST(TriangleTests, testPerimeter) {
+TEST(TriangleTests, testPerimeter2) {
     Triangle *aTriangle = new Triangle(3,3,2);
     EXPECT_EQ (aTriangle->getPerimeter(),8);
 }
 
-TEST(TriangleTests, testArea) {
+TEST(TriangleTests, testArea2) {
     Triangle *aTriangle = new Triangle(4,4,2);
     ASSERT_DOUBLE_EQ (3.87, aTriangle->getArea());
 }
