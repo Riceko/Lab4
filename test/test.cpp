76210c5bf234b6ea8a58fee622d841a22ffe7b40
Caleb Mak <calebmak@gmail.com>
2023-11-03 12:23:58 -0700

    update the AwardsTests

--- test/test.cpp
+++ test/test.cpp
@@ -7,5 +7,9 @@ TEST(AwardsTests, testWorks) {
     RankList list = new RankList;
     AwardCeremonyActions actions = new AwardCeremonyActions;
     EXPECT_NO_THROW(performAwardCeremony(list, actions));
-    
+    SHOULD_BE_CALLED(actions.playAnthem, 1);
+    SHOULD_BE_CALLED(actions.awardBronze, 1);
+    SHOULD_BE_CALLED(actions.awardSilver, 1);
+    SHOULD_BE_CALLED(actions.awardGold, 1);
+    SHOULD_BE_CALLED(actions.turnOffTheLightsAndGoHome, 1);
 }
