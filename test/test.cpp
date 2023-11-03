3da70ef716cf5c2e3b3e0c8c3576cd83de78c5c4
Caleb Mak <calebmak@gmail.com>
2023-11-03 14:20:02 -0700

    Fix triangle tests

--- test/test.cpp
+++ test/test.cpp
@@ -2,12 +2,12 @@
 #include "../include/Triangle.h"
 using shapes::Triangle;
 
-// 4 passing tests
 TEST(TriangleTests, testPerimeter) {
     Triangle *aTriangle = new Triangle(3,3,3);
     EXPECT_EQ (aTriangle->getPerimeter(),9);
 }
 
+// 4 passing tests
 TEST(TriangleTests, testIsosceles) {
     Triangle *aTriangle = new Triangle(5,3,3);
     bool isoceles = aTriangle->isIsosceles();
@@ -15,8 +15,10 @@ TEST(TriangleTests, testIsosceles) {
 }
 
 TEST(TriangleTests, testArea) {
-    Triangle *aTriangle = new Triangle(5,4,3);
-    EXPECT_EQ (aTriangle->getArea(),6);
+    Triangle *aTriangle = new Triangle(5,4,4);
+    // ASSERT_DOUBLE_EQ (7.806, aTriangle->getArea());
+    // ASSERT_NEAR(val1, val2, abs_error);
+    ASSERT_NEAR(aTriangle->getArea(), 7.806, 0.001);
 }
 
 TEST(TriangleTests, testEquilateral) {
@@ -25,24 +27,29 @@ TEST(TriangleTests, testEquilateral) {
     EXPECT_TRUE (equilateral);
 }
 
+TEST(TriangleTests, testPerimeter2) {
+    Triangle *aTriangle = new Triangle(10,10,10);
+    EXPECT_EQ (aTriangle->getPerimeter(),30);
+}
+
 // 1 using EXPECT_DEATH
 TEST(TriangleTests, testCreation) {
     Triangle *aTriangle;
-    EXPECT_DEATH (aTriangle = new Triangle(5,3,3), "Does not satisfy triangle inequality");
+    EXPECT_DEATH (aTriangle = new Triangle(5,3,1), "Does not satisfy triangle inequality");
 }
 
 // 3 failing tests
-TEST(TriangleTests, testPerimeter2) {
+TEST(TriangleTests, testPerimeter3) {
     Triangle *aTriangle = new Triangle(3,3,2);
     EXPECT_EQ (aTriangle->getPerimeter(),8);
 }
 
 TEST(TriangleTests, testArea2) {
     Triangle *aTriangle = new Triangle(4,4,2);
-    ASSERT_DOUBLE_EQ (3.87, aTriangle->getArea());
+    ASSERT_NEAR(aTriangle->getArea(), 3.87, 0.01);
 }
 
 TEST(TriangleTests, testKind) {
     Triangle *aTriangle = new Triangle(3,3,3);
-    EXPECT_EQ (aTriangle->getKind(),EQUILATERAL);
+    EXPECT_EQ (aTriangle->getKind(),Triangle::Kind::EQUILATERAL);
 }
